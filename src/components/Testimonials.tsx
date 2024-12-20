4a9f163e5b5ec3785b3a5b9051984ae651b29090
d-zucho <danny.nico.dev@outlook.com>
2024-12-19 16:24:43 -0800

    add testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,8 +4,20 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 const Testimonials = () => {
   return (
     <section className='mb-20'>
-      <MaxWidthWrapper>
-        <FiveStarRating />
+      <MaxWidthWrapper className='lg:text-center lg:flex lg:flex-col lg:justify-center'>
+        <div className='lg:flex lg:justify-center'>
+          <FiveStarRating />
+        </div>
+        <div>
+          <p className='text-[24px] font-semibold text-neutral-900 tracking-[-1px] leading-[110%] sm:text-[34px] my-8'>
+            "This book club transformed my technical reading from a solitary
+            activity into an enriching community experience. The discussions are
+            gold!"
+          </p>
+        </div>
+        <p className='font-inter text-xl tracking-tight leading-[140%]'>
+          Sarah Chen, Software Architect
+        </p>
       </MaxWidthWrapper>
     </section>
   )
